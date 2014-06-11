71b324231cab3fa3ade0d7018c8c692cab527064
Megaf <mmegaf@gmail.com>
2014-06-11 11:18:51 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ minetest.register_node("green_screen:greenscreen", {
 description = "Green Screen Background",
 tiles = {"green_screen.png"},
 is_ground_content = true,
+paramtype = "light",
 groups = {cracky=3,oddly_breakable_by_hand=3},
 drop = 'green_screen:greenscreen',
 })
